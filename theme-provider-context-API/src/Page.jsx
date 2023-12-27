f4617a72be611b9b9a7b7ec090e63f1a922f98ac
MaNaSDeo <102231033+MaNaSDeo@users.noreply.github.com>
2023-12-27 13:13:05 +0530

    without context

---
+++ theme-provider-context-API/src/Page.jsx
@@ -0,0 +1,16 @@
+import Header from './Header';
+
+export default function Page({theme, toggleTheme}) {
+  return (
+    <div id="app" className={theme}>
+      <Header toggleTheme={toggleTheme} />
+
+      <article>
+        <h2>React Course</h2>
+        <p>
+          A course that teaches you React from the ground up and in great depth!
+        </p>
+      </article>
+    </div>
+  );
+}
